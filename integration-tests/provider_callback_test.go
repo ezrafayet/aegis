2f30ce64f241ff7db0be3435e170e5113741c73f
Ezra Fayet <evrard.fayet.feret@gmail.com>
2025-07-12 22:29:04 +0300

    added test

--- integration-tests/provider_callback_test.go
+++ integration-tests/provider_callback_test.go
@@ -1,10 +1,36 @@
 package integration
 
-import "testing"
+import (
+	"aegis/integration-tests/testkit"
+	"aegis/pkg/apperrors"
+	"encoding/json"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestProviderCallback(t *testing.T) {
 	t.Run("unhappy scenarios: invalid data / generic", func(t *testing.T) {
-		t.Run("calling GET /provider/callback returns an error if the provider is not enabled", func(t *testing.T) {})
+		t.Run("calling GET /provider/callback returns 403 if the provider is not enabled", func(t *testing.T) {
+			config := testkit.GetBaseConfig()
+			config.Auth.Providers.GitHub.Enabled = false
+			suite := testkit.SetupTestSuite(t, config)
+			defer suite.Teardown()
+
+			resp, err := http.Get(suite.Server.URL + "/auth/github/callback")
+			require.NoError(t, err)
+			assert.Equal(t, http.StatusForbidden, resp.StatusCode)
+
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			var errorResponse map[string]interface{}
+			err = json.Unmarshal(body, &errorResponse)
+			require.NoError(t, err)
+			assert.Contains(t, errorResponse["error"], apperrors.ErrAuthMethodNotEnabled.Error())
+		})
 		t.Run("calling GET /provider/callback with invalid state gets rejected", func(t *testing.T) {})
 		t.Run("calling GET /provider/callback with invalid code gets rejected", func(t *testing.T) {})
 	})
